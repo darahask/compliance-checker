bc136e58b3985927be426c0100f8bb0efe43f36c
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2021-11-29 17:20:40 +0530

    all urls parsed

--- browser.js
+++ browser.js
@@ -29,6 +29,30 @@ module.exports = instance = async (host) => {
   var consent = RegExp('Cookie','i').test(extractedText.trim());
   console.log("The browser has cookie consent",consent)
   
+  const urls = await page.evaluate(()=>{
+    var urlList = [];
+    data = document.getElementsByTagName('a')
+    for(const [key,val] of Object.entries(data)){
+      urlList.push(val.href)
+    }
+    return urlList
+  })
+
+  console.log(urls)
   
   await browser.close();
 };
+
+function getLinksFromHtml(htmlString) {
+  // Regular expression that matches syntax for a link (https://stackoverflow.com/a/3809435/117030):
+  LINK_REGEX = "/https?://(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi";
+
+  // Use the regular expression from above to find all the links:
+  matches = htmlString.match(LINK_REGEX);
+
+  // Output to console:
+  console.log(matches);
+
+  // Alternatively, return the array of links for further processing:
+  return matches;
+}
